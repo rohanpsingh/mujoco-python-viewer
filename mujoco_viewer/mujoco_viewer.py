0942bed788e20f2274e4e962a36b86f05c096120
LuanGBR <luangustavodebrito@gmail.com>
2022-11-12 20:16:14 -0300

    changed return to tuple intead of 4d-matrix

--- mujoco_viewer/mujoco_viewer.py
+++ mujoco_viewer/mujoco_viewer.py
@@ -301,7 +301,7 @@ class MujocoViewer(Callbacks):
             depth_img = np.zeros((shape[1], shape[0], 1), dtype=np.float32)
             mujoco.mjr_readPixels(img, None, self.viewport, self.ctx)
             mujoco.mjr_readPixels(None, depth_img, self.viewport, self.ctx)
-            return np.dstack((np.flipud(img),np.flipud(depth_img)))
+            return (np.flipud(img),np.flipud(depth_img))
         else:
             img = np.zeros((shape[1], shape[0], 3), dtype=np.uint8)
             mujoco.mjr_readPixels(img, None, self.viewport, self.ctx)
